16697f5f143cbc17ad55e2aec72044d6f2c9470e
princewillopah <princewillopah@gmail.com>
2024-01-24 12:06:56 +0000

    new push

--- my-ansible-slaves-setup/variables.tf
+++ my-ansible-slaves-setup/variables.tf
@@ -1,6 +1,6 @@
 variable "avail_zone" {
  type        = list(string)
- description = "Availability Zones"
+ description = "Availability Zones!!!!"
  default     = ["eu-north-1a", "eu-north-1b"]
 }
 variable "env_prefix" {

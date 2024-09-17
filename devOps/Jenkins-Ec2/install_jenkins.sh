a4757f27a10de9aa30896950d84ae258135cb460
princewillopah <princewillopah@gmail.com>
2024-09-17 08:22:19 +0100

    new push

---
+++ devOps/Jenkins-Ec2/install_jenkins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+exec > >(tee -i /var/log/user-data.log)
+exec 2>&1
+sudo apt update -y
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+sudo apt install git -y 
+mkdir Ansible && cd Ansible  ## make this "Ansible" dirctory in the newly provisioned Jenkins Instance
+pwd
+git clone https://github.com/princewillopah/Ansible.git
+cd Ansible/ANSIBLE   # this is where DevSecOps.yml is located.
+ansible-playbook -i localhost DevSecOps.yml

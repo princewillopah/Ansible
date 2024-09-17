d8cc8435858c9345b512d851a8843a0025879508
princewillopah <princewillopah@gmail.com>
2024-09-17 07:51:59 +0100

    new push

--- git-push.sh
+++ git-push.sh
@@ -2,7 +2,7 @@
 
 
 # Ask the user for the project name (root folder name)
-read -p "Pls, Enter Commit Message Here: " COMMIT_MESSAGE
+read -p "Kindly Enter Commit Message Here: " COMMIT_MESSAGE
 
 # Set the commit message
 COMMIT_MESSAGE="$COMMIT_MESSAGE_$(date +%d-%m-%Y_%H-%M-%S)"
@@ -18,3 +18,5 @@ git commit -m "$COMMIT_MESSAGE"
 
 # Push the changes to the GitHub repository
 git push origin master  # Change 'master' to your branch name if different
+
+
